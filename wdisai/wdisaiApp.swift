d66fcf4c37dcea2a5c66a61346ca6dc876403da1
spak2005 <uzoisrael2005@gmail.com>
2026-02-18 17:22:41 -0600

    Initial Commit

---
+++ wdisai/wdisaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  wdisaiApp.swift
+//  wdisai
+//
+//  Created by Israel Ogbonna on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct wdisaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wdisaiTests/wdisaiTests.swift
@@ -0,0 +1,17 @@
+//
+//  wdisaiTests.swift
+//  wdisaiTests
+//
+//  Created by Israel Ogbonna on 2/18/26.
+//
+
+import Testing
+@testable import wdisai
+
+struct wdisaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
